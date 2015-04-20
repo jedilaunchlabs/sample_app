74963527af63e6c7a663826bd18c4613a4991a4a
Jedi Ballono <jedi@launchlabs.ph>
2015-04-20 11:52:35 +0800

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,8 @@ ActiveRecord::Schema.define(version: 20150417104433) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "password_disgest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
 
