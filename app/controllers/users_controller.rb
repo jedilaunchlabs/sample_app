61baa2a189e6bc8ad79998f8fa972c6038f89f6d
Jedi Ballono <jedi@launchlabs.ph>
2015-04-21 16:14:49 +0800

    Not editable through web (solves exercise 9.3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
 
   	def user_params
   		params.require(:user).permit(:name, :email,
-  									 :password, :password_confirmation)
+  									 :password, :password_confirmation, :admin)
   	end	
 
     # Before filters
