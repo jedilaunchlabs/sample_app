9244da7c60c290cd073741286b3d85a514ff9c41
Jedi Ballono <jedi@launchlabs.ph>
2015-04-21 10:12:32 +0800

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 									 			  password_confirmation: 	"password"}
 		end
 		assert_template 'users/show'
+
+		assert is_logged_in?
 		
 		assert_not flash.nil?
 	end
